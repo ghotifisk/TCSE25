23c4a05fdd6ec49a67692e965354419a407b7de7
Einar Smith <124695285+ghotifisk@users.noreply.github.com>
2023-02-09 10:50:14 +0100

    chap3

---
+++ untitled text 5.py
@@ -0,0 +1,20 @@
+# The largest heading
+## The second largest heading
+###### The smallest heading x
+
+p.19 Rice and Chessboard
+
+```ruby
+require 'redcarpet'
+markdown = Redcarpet.new("Hello World!")
+puts markdown.to_html
+```
+
+```python
+s = 0; n = 1; fv = 1
+while n < 65:
+    s = s + fv
+    n = n +1
+    fv = 2 * fv
+print(s)
+```
